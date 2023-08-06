d75c67fbf2248192e5184689f7b19229e9cfcbdc
Christian <97773844+esuriru@users.noreply.github.com>
2023-08-07 01:47:15 +0800

    Added comments.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Hamburger nav menu
 const hamburgerNavMenu = document.getElementById("hamburger-nav-menu");
 
 // Media query
@@ -7,24 +8,20 @@ function FixResponsive(e)
 {
     if (e.matches)
     {
-        const wrapper = document.querySelector(".fade-wrapper.active");
-        if (wrapper == null) return;
-        const body = wrapper.children[0];
-        body.classList.add("active");
+        // Mobile fixes.
         SelectVideo();
-    }
-    else
-    {
-        const body = document.querySelector("div.active");
-        body.parentElement.classList.add("active");
     }
 }
 
+// When media query changes, call fix responsive
 mediaQuery.addListener(FixResponsive);
 
+// get all the pieces on html file and convert to array.
 const pieces = Array.from(document.querySelectorAll(".chess-piece"));
 var currentSelectedPiece = 0;
 const pieceNames = [ "King", "Queen", "Rook", "Bishop", "Knight", "Pawn" ];
+
+// Videos for each piece in order of array pieceNames
 const pieceVideos = 
 [
     document.getElementById("king-video"),
@@ -33,7 +30,9 @@ const pieceVideos =
     document.getElementById("bishop-video"),
     document.getElementById("knight-video"),
     document.getElementById("pawn-video"),
-]
+];
+
+// Descriptions for each piece in order of array pieceNames
 const pieceDescriptions = 
 [ 
     "The king can only move 1 step in all directions.",
@@ -45,6 +44,8 @@ const pieceDescriptions =
 ];
 const pieceHeader = document.getElementById("piece-section-header");
 const pieceDesc = document.getElementById("piece-description");
+
+// Class names for pieces type
 const pieceElementClassNames = 
 [
     ".chess-board-king"  ,
@@ -55,6 +56,7 @@ const pieceElementClassNames =
     ".chess-board-pawn"  ,
 ];
 
+// class name for piece's individual starting positions
 const pieceElementStartingPositionClassNames = 
 [
     ".king-starting-position"  ,
@@ -65,6 +67,7 @@ const pieceElementStartingPositionClassNames =
     ".pawn-starting-position"  ,
 ];
 
+// Movement values for king
 const kingMovement =
 [
     [0, 1],
@@ -77,6 +80,7 @@ const kingMovement =
     [-1, -1],
 ];
 
+// Movement values for knight
 const knightMovement = 
 [
     [1, 2],
@@ -87,20 +91,23 @@ const knightMovement =
     [-1, -2],
     [-2, 1],
     [-2, -1],
-]
+];
 
 var pawnHasMoved = false;
 
+// Numeric check
 function IsNumeric(str) {
     if (typeof str != "string") return false; 
     return !isNaN(str) && !isNaN(parseFloat(str));
 }
 
+// In bounds check.
 function InBounds(row, column)
 {
     return row >= 1 && row <= 8 && column >= 1 && column <= 8;
 }
 
+// Hamburger menu toggle
 function OnHamburgerMenuClick()
 {
     hamburgerNavMenu.classList.toggle("open");
@@ -108,6 +115,7 @@ function OnHamburgerMenuClick()
 
 function SelectVideo()
 {
+    // Update the video based on current selected piece.
     for (let i = 0; i < pieceVideos.length; ++i)
     {
         pieceVideos[i].style.display = i == currentSelectedPiece ? "block" : "none";
@@ -116,13 +124,20 @@ function SelectVideo()
 
 function ResetPositions()
 {
+    // Get all active indicators and then remove their active status.
     document.querySelectorAll(".chess-board-indicator.active").forEach(i => i.classList.remove("active"));
+
+    // Get selected item and then remove its selected class.
     const selected = document.querySelector(".selected");
     if (selected != null)
         selected.classList.remove("selected");
+
+    // Reset pawn movement.
     pawnHasMoved = false;
+
     var pieceElements = [];
     var pieceStartingElements = [];
+
     for (let i = 0; i < pieceElementClassNames.length; ++i)
     {
         pieceElements.push(document.querySelector(pieceElementClassNames[i]));
@@ -132,16 +147,17 @@ function ResetPositions()
     for (let i = 0; i < pieceElements.length; ++i)
     {
         if (pieceStartingElements[i] == null || pieceElements[i] == null) continue;
-        console.log(pieceElements[i]);
-        // while (pieceElements[i].childNodes.length > 0)
-        // {
-        //     pieceStartingElements[i].appendChild(pieceElements[i].childNodes[0]);
-        // }
+
+        // Put the piece element back in where it belongs.
         pieceStartingElements[i].appendChild(pieceElements[i]);
+
+        // Get the row and column of the indicator based on its position in the parent.
         const piece = pieceElements[i];
-        const row = piece.parentElement.parentElement.id.slice(4);
+        const row = piece.parentElement.parentElement.id.slice(4); // 'row-x'
         const rowElement = piece.parentElement.parentElement;
         const column = Array.prototype.indexOf.call(rowElement.children, piece.parentElement);
+
+        // Get all numbers from class list and remove them.
         for (let i = 0; i < piece.classList.length; ++i)
         {
             if (IsNumeric(piece.classList.item(i)))
@@ -151,6 +167,7 @@ function ResetPositions()
             }
         }
 
+        // Prepend new position numbers into class list of the defaulted piece.
         var newClassList = Array.from(piece.classList);
         newClassList.unshift(row + column.toString());
         piece.className = "";
@@ -168,6 +185,7 @@ function UpdateChessBoardOnSelect(reset = true)
         ResetPositions();
     }
 
+    // Select the video based on new piece.
     SelectVideo();
     var pieceElements = [];
     for (let i = 0; i < pieceElementClassNames.length; ++i)
@@ -175,6 +193,7 @@ function UpdateChessBoardOnSelect(reset = true)
         pieceElements.push(document.querySelector(pieceElementClassNames[i]));
     }
 
+    // Set all the piece elements to invisible.
     for (let i = 0; i < pieceElements.length; ++i)
     {
         if (pieceElements[i] != null)
@@ -182,12 +201,15 @@ function UpdateChessBoardOnSelect(reset = true)
             pieceElements[i].style.display = 'none';
         }
     }
+
+    // Set the current selected piece to visible.
     if (pieceElements[currentSelectedPiece] != null)
     {
-        pieceElements[currentSelectedPiece].style.display = 'inline'
+        pieceElements[currentSelectedPiece].style.display = 'inline';
     }
 }
 
+// Update everything once a new piece has been selected. 
 function UpdatePieceSection()
 {
     pieceHeader.textContent = pieceNames[currentSelectedPiece];
@@ -206,6 +228,7 @@ function UpdatePieceSection()
     }
 }
 
+// Left Selector callback
 function SelectPieceOnLeft()
 {
     if (currentSelectedPiece == 0)
@@ -219,6 +242,7 @@ function SelectPieceOnLeft()
     UpdatePieceSection();
 }
 
+// Right Selector callback
 function SelectPieceOnRight()
 {
     if (currentSelectedPiece == pieces.length - 1)
@@ -257,27 +281,27 @@ function Reveal()
 function GoToOpenings()
 {
     const openings = document.getElementById("openings-page");
-    const allActive = document.querySelectorAll(".active");
+
+    // If openings is already active, early return.
+    if (openings.parentElement.classList.contains("active"))
+    {
+        return;
+    }
+
+    // If on desktop, do transition
     if (!mediaQuery.matches)
     {
-        if (openings.parentElement.classList.contains("active"))
-        {
-            return;
-        }
-
         const allActive = document.querySelectorAll(".active");
+        // Scroll out first.
         for (let i = 0; i < allActive.length; ++i)
         {
             allActive[i].classList.remove("active");
         }
+        // After first transition done, do our requested transition 
         setTimeout(function()
         {
             openings.parentElement.classList.add("active");
         }, 600);
-        // setTimeout(function()
-        // {
-        //     history.parentElement.classList.add("active");
-        // }, 200);
     }
     else
     {
@@ -293,27 +317,24 @@ function GoToOpenings()
 function GoToHistory()
 {
     const history = document.getElementById("history-page");
-    const allActive = document.querySelectorAll(".active");
+    // If history is already active, early return.
+    if (history.parentElement.classList.contains("active"))
+    {
+        return;
+    }
+    // If on desktop, do transition
     if (!mediaQuery.matches)
     {
-        if (history.parentElement.classList.contains("active"))
-        {
-            return;
-        }
-
         const allActive = document.querySelectorAll(".active");
         for (let i = 0; i < allActive.length; ++i)
         {
             allActive[i].classList.remove("active");
         }
+        // After first transition done, do our requested transition 
         setTimeout(function()
         {
             history.parentElement.classList.add("active");
         }, 600);
-        // setTimeout(function()
-        // {
-        //     history.parentElement.classList.add("active");
-        // }, 200);
     }
     else
     {
@@ -335,31 +356,20 @@ function GoToRules()
     if (rules.parentElement.classList.contains("active")) return;
 
     const allActive = document.querySelectorAll(".fade-wrapper.active");
-    // setTimeout(function()
-    // {
-    //     for (let i = 0; i < allActive.length; ++i)
-    //     {
-    //         allActive[i].classList.remove("active");
-    //     }
-    // }, 990);
 
-
+    // If on desktop, do transition
     if (!mediaQuery.matches)
     {
+        // Scroll out first.
         for (let i = 0; i < allActive.length; ++i)
         {
             allActive[i].classList.remove("active");
-            // setTimeout(function(){
-            //     if (allActive[i].children[0].classList.contains("active"))
-            //     allActive[i].children[0].classList.remove("active");
-
-            // }, 200);
         }
         UpdateChessBoardOnSelect(); 
+        // After first transition done, do our requested transition 
         setTimeout(function()
         {
             rules.parentElement.classList.add("active");
-            // rules.classList.add("active");
         }, 500);
     }
     else
@@ -376,8 +386,8 @@ function GoToRules()
 function GoToHome()
 {
     const home = document.getElementById("about-body");
-    const allActive = document.querySelectorAll(".active");
 
+    // If on desktop, do transition
     if (!mediaQuery.matches)
     {
         if (home.parentElement.classList.contains("active"))
@@ -385,15 +395,17 @@ function GoToHome()
             return;
         }
 
+        // Scroll out first.
         const allActive = document.querySelectorAll(".active");
         for (let i = 0; i < allActive.length; ++i)
         {
             allActive[i].classList.remove("active");
         }
+        // After first transition done, do our requested transition 
         setTimeout(function()
         {
             home.parentElement.classList.add("active");
-        }, 600);
+        }, 500);
 
     }
     else
@@ -413,8 +425,15 @@ function OnQueenClick(button)
     console.log("test");
     ButtonSelect(button);
 
+    // Get current position
     let currentRow = parseInt(button.classList[0][0]);
     let currentColumn = parseInt(button.classList[0][1]);
+
+    // The reason why we want to use 8 is because in case they are in a corner, it would need to go to the end corner.
+    // Not the most efficient solution but should be ok since we are breaking once 
+    // they are out of bounds in said specific for loop
+    // Queen is just bishop and rook combined.
+    // Iterate through all possible relative positions and check if its in bounds, then set the indicators to active.
 
     for (let i = 1; i < 8; ++i)
     {
@@ -536,8 +555,14 @@ function OnRookClick(button)
 {
     ButtonSelect(button);
 
+    // Get current position
     let currentRow = parseInt(button.classList[0][0]);
     let currentColumn = parseInt(button.classList[0][1]);
+
+    // The reason why we want to use 8 is because in case they are in a corner, it would need to go to the end corner.
+    // Not the most efficient solution but should be ok since we are breaking once 
+    // they are out of bounds in said specific for loop
+    // Iterate through all possible relative positions and check if its in bounds, then set the indicators to active.
 
     for (let i = 1; i < 8; ++i)
     {
@@ -601,9 +626,14 @@ function OnBishopClick(button)
 {
     ButtonSelect(button);
 
+    // Get current position
     let currentRow = parseInt(button.classList[0][0]);
     let currentColumn = parseInt(button.classList[0][1]);
 
+    // The reason why we want to use 8 is because in case they are in a corner, it would need to go to the end corner.
+    // Not the most efficient solution but should be ok since we are breaking once 
+    // they are out of bounds in said specific for loop
+    // Iterate through all possible relative positions and check if its in bounds, then set the indicators to active.
     for (let i = 1; i < 8; ++i)
     {
         const row = currentRow + i;
@@ -671,9 +701,9 @@ function OnKingClick(button)
 
     for (let i = 0; i < kingMovement.length; ++i)
     {
+        // Iterate through all possible relative positions and check if its in bounds, then set the indicators to active.
         const row = currentRow + kingMovement[i][0];
         const column = currentColumn + kingMovement[i][1];
-        // console.log(row + ", ", column);
         if (InBounds(row, column))
         {
             GetIndicator(row, column).classList.toggle("active");
@@ -685,15 +715,15 @@ function OnKnightClick(button)
 {
     ButtonSelect(button);
 
+    // Get current position from class list.
     let currentRow = parseInt(button.classList[0][0]);
     let currentColumn = parseInt(button.classList[0][1]);
-    // console.log(button.classList);
 
     for (let i = 0; i < knightMovement.length; ++i)
     {
+        // Iterate through all possible relative positions and check if its in bounds, then set the indicators to active.
         const row = currentRow + knightMovement[i][0];
         const column = currentColumn + knightMovement[i][1];
-        // console.log(row + ", ", column);
         if (InBounds(row, column))
         {
             GetIndicator(row, column).classList.toggle("active");
@@ -704,13 +734,16 @@ function OnKnightClick(button)
 
 function GetIndicator(row, column)
 {
+    // Get the indicator on specific row and column using the parent's ID which is 'row-x' based on row number.
     const nextRowElement = document.getElementById("row-".concat(row.toString()));
     const nextElement = nextRowElement.children.item(column);
+    // Get the indicator from the parent row. 
     return nextElement.querySelector(".chess-board-indicator");
 }
 
 function ButtonSelect(button)
 {
+    // Check if the button is already selected, if not, remove all other selected buttons before selecting.
     if (!button.classList.contains("selected"))
         document.querySelectorAll(".selected").forEach(s => s.classList.remove("selected"));
     button.classList.toggle("selected");
@@ -719,10 +752,13 @@ function ButtonSelect(button)
 function OnPawnClick(button)
 {
     ButtonSelect(button);
+    
+    // Get current position from class list.
     let currentRow = parseInt(button.classList[0][0]);
     let currentColumn = parseInt(button.classList[0][1]);
     console.log(button.classList[0]);
 
+    // Check if current row is valid to add on. 
     if (currentRow < 8)
     {
         currentRow++;
@@ -730,6 +766,8 @@ function OnPawnClick(button)
 
     GetIndicator(currentRow, currentColumn).classList.toggle("active");
 
+    // Since we know that the limit is the end and the pawn would have had to definitely move first, we do not need
+    // to check whether the current row + 1 is correct for this specific case 
     if (!pawnHasMoved)
     {
         GetIndicator(currentRow + 1, currentColumn).classList.toggle("active");
@@ -738,31 +776,35 @@ function OnPawnClick(button)
 
 function OnIndicatorClick(indicator)
 {
+    // If there are no selected pieces (this should never be called in the first place) return early.
     const pieces = document.querySelectorAll(".selected");
     if (pieces.length == 0)
     {
         return;
     }
 
-    const row = indicator.parentElement.parentElement.id.slice(4);
+    // Get the row and column of the indicator based on its position in the parent.
+    const row = indicator.parentElement.parentElement.id.slice(4); // The parent ID is 'row-x' so we just want x
     const rowElement = indicator.parentElement.parentElement;
     const column = Array.prototype.indexOf.call(rowElement.children, indicator.parentElement);
 
-    const piece = pieces[0];
+    const piece = pieces[0]; 
 
+    // If it is a pawn, the first move should be set.
     if (!pawnHasMoved && piece.classList.contains("chess-board-pawn"))
     {
         pawnHasMoved = true;
     }
 
     indicator.parentElement.appendChild(piece);
-    // piece.parentElement.removeChild(piece);
-    // piece.parentElement.innerHTML = piece.parentElement.innerHTML.slice(piece.outerHTML.length);
+
+    // Get all the new selected pieces and unselect them
     const piecesAfter = document.querySelectorAll(".selected");
     if (piecesAfter != null && piecesAfter.length > 0)
     {
         piecesAfter[0].classList.remove("selected");
     }
+    // Get all the active indicators and make them not active by removing their active class
     document.querySelectorAll(".chess-board-indicator.active").forEach(i => i.classList.remove("active"));
 
 
@@ -776,9 +818,7 @@ function OnIndicatorClick(indicator)
         }
     }
 
-    // var pieceClassList = piecesAfter[0].classList;
-    // pieceClassList.unshift(row, column.toString());
-    // piecesAfter[0].classList = pieceClassList;
+    // Add the new numbers to the front of the array using unshift and put them back into the class list.
     var newClassList = Array.from(piece.classList);
     newClassList.unshift(row + column.toString());
     piece.className = "";
@@ -792,6 +832,8 @@ function OnIndicatorClick(indicator)
 
 
 const quizResult = document.getElementById("quiz-result");
+
+// Function to check and set the result of the quiz.
 function GetResults()
 {
     var italian = document.getElementById("italian");
@@ -805,6 +847,7 @@ function GetResults()
     }
     else
     {
+        // If none of the elements are checked, return early so nothing happens.
         if (!italian.checked && !french.checked) return;
         quizResult.textContent = "You are wrong!";
         quizResult.style.color = "var(--red)";
@@ -813,7 +856,11 @@ function GetResults()
 
 
 window.addEventListener("scroll", Reveal);
+
+// Update reveal on load.
 Reveal();
+
+// Update chessboard on load.
 UpdateChessBoardOnSelect();
 
 
